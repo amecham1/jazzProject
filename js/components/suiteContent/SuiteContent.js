6c9b783b1f0bc7021889b117c8bb2c9a08060344
Andrew <andrew.mecham@gmail.com>
2017-02-03 20:59:31 -0700

    began working on content found solution to hide and show content

---
+++ js/components/suiteContent/SuiteContent.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './suiteContent.scss'
+
+class SuiteContent extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      view: true
+    }
+  }
+  viewContent () {
+    if (!this.props.containerStatus) {
+      this.state.view = false
+      console.log(this.state.view)
+      return 'close-content'
+    } else {
+      this.state.view = true
+      console.log(this.state.view)
+      return 'open-content'
+    }
+  }
+  render () {
+    let showContainer = this.viewContent()
+    return (
+      <div >
+        <p className={showContainer}>is hkjhkjhjk working</p>
+      </div>
+    )
+  }
+}
+SuiteContent.propTypes = {
+  containerStatus: React.PropTypes.bool
+}
+
+export default SuiteContent
--- js/components/suiteTitle/SuiteTitle.js
+++ js/components/suiteTitle/SuiteTitle.js
@@ -1,19 +1,23 @@
 import React from 'react'
+import SuiteContent from '../suiteContent/SuiteContent.js'
 import './suite-title.scss'
 
 class SuiteTitle extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
+      // state for setting the height of suite div
       open: false
     }
     this.toggleContent = this.toggleContent.bind(this)
   }
+  // allows the user to open and close the suite div
   toggleContent () {
     this.setState({
       open: !(this.state.open)
     })
   }
+  // sets the height when opened and closed
   getContentToggleHeight () {
     if (this.state.open) {
       return '80vh'
@@ -21,6 +25,7 @@ class SuiteTitle extends React.Component {
       return '10vh'
     }
   }
+  // sets the backgroung color when opened and closed
   changeBackgroundColor () {
     if (this.state.open) {
       return '#FFFFFF'
@@ -28,15 +33,25 @@ class SuiteTitle extends React.Component {
       return '#F89F1F'
     }
   }
+  // hide the content until opened
+  revealContent () {
+    if (this.state.open) {
+      return this.state.open
+    } else {
+      return this.state.open
+    }
+  }
   render () {
-    var contentStyle = { height: this.getContentToggleHeight(),
-    'background-color': this.changeBackgroundColor() }
+    let contentStyle = { height: this.getContentToggleHeight(),
+    backgroundColor: this.changeBackgroundColor() }
+    // let showContent = { display: this.revealContent() }
     return (
       <div style={contentStyle} className='suite-title suite-title-begin' onClick={this.toggleContent}>
         <p className='suite-name'>LUXURY SUITES</p>
         <div className='down-arrow-box'>
           <img className='down-arrow' src='js/img/Down Arrow.svg' alt='down facing arrowhead' />
         </div>
+        <SuiteContent containerStatus={this.state.open} />
       </div>
     )
   }
